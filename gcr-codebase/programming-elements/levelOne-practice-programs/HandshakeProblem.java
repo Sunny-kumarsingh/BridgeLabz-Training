d6aff41a58d4cbdb82fbc388b159a8e90e093ce2
Sunny-kumarsingh <sunnykumarsingh2711@gmail.com>
2025-12-17 22:57:35 +0530

    [Sunny] : added possible Handshake

---
+++ gcr-codebase/programming-elements/levelOne-practice-programs/HandshakeProblem.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Handshakes {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take number of students
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = sc.nextInt();
+
+        // Calculate handshakes
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        // Output
+        System.out.println("The maximum number of possible handshakes is " + handshakes);
+	
+    }
+}
