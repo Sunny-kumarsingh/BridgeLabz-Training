b0f91ad5c15bfa22c3af0b7a1a5d2a8676ccb301
Sunny-kumarsingh <sunnykumarsingh2711@gmail.com>
2025-12-17 15:48:37 +0530

    [Sunny] : added Sam's averageMark

---
+++ gcr-codebase/programming-elements/levelOne-practice-programs/AvgResult.java
@@ -0,0 +1,11 @@
+public class AvgResult{
+	public static void main(String args[]){
+		int mathsMark = 94;
+		int phyMark = 95;
+		int chemMark = 96;
+		double totalAvgMark = (mathsMark + phyMark + chemMark)/3;
+		System.out.println("sam's average mark in PCM is " + totalAvgMark );
+		
+	}
+
+}
