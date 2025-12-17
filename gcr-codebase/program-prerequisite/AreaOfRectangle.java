ba78b80d965edcbf454dd9705f268b1ec14ee246
Sunny-kumarsingh <sunnykumarsingh2711@gmail.com>
2025-12-17 11:31:11 +0530

    [Sunny] : added Area of Rectangle

---
+++ gcr-codebase/program-prerequisite/AreaOfRectangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class AreaOfRectangle{
+	public class static void main(String args[]){
+	System.out.println("Enter the length and width");
+	Scanner sc = new Scanner(System.in);
+	int l = sc.nextInt();
+	int w = sc.nextInt();
+	int area = 2*(l+w);
+	System.out.println("Area of Rectangle : " + area);
+
+}
+
+}
