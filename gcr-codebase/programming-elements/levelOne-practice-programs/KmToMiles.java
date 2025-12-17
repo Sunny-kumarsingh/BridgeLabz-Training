9098ffd85f095410a16111fd08bb252690bf5389
Sunny-kumarsingh <sunnykumarsingh2711@gmail.com>
2025-12-17 17:11:37 +0530

    [Sunny] : added km to miles by taking user input

---
+++ gcr-codebase/programming-elements/levelOne-practice-programs/KmToMiles.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class KmToMiles {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter distance in kilometers: ");
+        double km;
+        km = input.nextInt();
+
+        double miles = km / 1.6;
+
+        // Print output
+        System.out.println("The total miles is " + miles + 
+                " mile for the given " + km + " km");
+
+    }
+}
