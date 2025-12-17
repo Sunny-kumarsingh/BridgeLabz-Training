36a46fb5f53644003cf606339627d8377125242c
Sunny-kumarsingh <sunnykumarsingh2711@gmail.com>
2025-12-17 15:10:34 +0530

    [Sunny] : added Harry Age

---
+++ gcr-codebase/programming-elements/levelOne-practice-programs/HarryAge.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HarryAge{
+	public static void main(String args[]){
+		// hari birth year
+		int birthYear = 2000;
+		
+		// current year
+		int currYear = 2024;
+		System.out.println("Harry's age in "+ currYear + " is "+ (currYear-birthYear));
+	}
+}
