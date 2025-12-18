7b8ec853d1ea5db543b52365ade68639d6c644e3
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-18 12:09:17 +0530

    [Sunny] : added by mistake I added this one also in level One

---
+++ gcr-codebase/programming-elements/levelTwo-practice-programs/IntOperation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class IntOperation {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take user input for a, b, and c
+        System.out.print("Enter value of a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter value of b: ");
+        int b = sc.nextInt();
+        System.out.print("Enter value of c: ");
+        int c = sc.nextInt();
+
+        // Integer operations
+        int result1 = a + b * c;
+        int result2 = a * b + c;
+        int result3 = c + a / b;
+        int result4 = a % b + c;
+
+        // Print output
+        System.out.println("The results of Int Operations are " 
+                + result1 + ", " + result2 + ", " 
+                + result3 + ", and " + result4);
+
+    }
+}
