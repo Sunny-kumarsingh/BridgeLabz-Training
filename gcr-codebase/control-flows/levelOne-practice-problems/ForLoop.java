922b0b78a99106bf3f465a928a6e9d344c96cff4
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-18 20:15:07 +0530

    [Sunny] : added For Loop

---
+++ gcr-codebase/control-flows/levelOne-practice-problems/ForLoop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+// rewrite the problem no. 8 using For-Loop
+public class ForLoop {
+    public static void main(String[] args) {
+	
+		//Creating object
+        Scanner s = new Scanner(System.in);
+		
+		//take input
+        System.out.print("Enter number to start countdown: ");
+        int counter = s.nextInt();
+
+		//count the counter
+        for (int i = counter; i >= 1; i--) {
+            System.out.println(i);
+        }
+		
+        s.close();
+    }
+}
