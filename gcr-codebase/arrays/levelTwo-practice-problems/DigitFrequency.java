21115fbc9fd7d2087fc9fb37af7f949a24d67d42
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-20 12:58:21 +0530

    [Sunny] : added Digit Frequency

---
+++ gcr-codebase/arrays/levelTwo-practice-problems/DigitFrequency.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        // Create a Scanner Object 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int temp = number;
+        int count = 0;
+
+        // count digits
+        while(temp != 0){
+            count++;
+            temp = temp / 10;
+        }
+        int[] digits = new int[count];
+        temp = number;
+        // store digits in array
+        for(int i=0;i<count;i++){
+            digits[i] = temp % 10;
+            temp = temp / 10;
+        }
+
+        int[] frequency = new int[10];
+
+        // find frequency
+        for(int i=0;i<count;i++){
+            frequency[digits[i]]++;
+        }
+
+        // display frequency
+        System.out.println("Digit Frequency:");
+        for(int i=0;i<10;i++){
+            if(frequency[i] > 0){
+                System.out.println(i+" -> "+frequency[i]);
+            }
+        }
+    }
+}
