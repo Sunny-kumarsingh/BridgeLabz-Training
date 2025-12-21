13fcc3027f0fb50721a40396e1400ddf882eeba5
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-21 19:08:16 +0530

    [Sunny] : added Unit Unit Convertor Two

---
+++ gcr-codebase/methods/levelTwo-practice-problems/UnitConvertorTwo.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class UnitConvertorTwo {
+
+    // Method to convert yards to feet
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    // Method to convert feet to yards
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    // Method to convert meters to inches
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    // Method to convert inches to meters
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    // Method to convert inches to centimeters
+    public static double convertInchesToCm(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+
+    public static void main(String[] args) {
+        
+        double yards = 10;
+        double feet = convertYardsToFeet(yards);
+        System.out.println(yards + " yards = " + feet + " feet");
+
+        double meters = 1;
+        System.out.println(meters + " meter = " + convertMetersToInches(meters) + " inches");
+    }
+}
