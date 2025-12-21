277250ed3227c8842c23b29f52b69eb105811524
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-21 09:36:18 +0530

    [Sunny] : added Chocolates Distribution

---
+++ gcr-codebase/methods/levelOne-practice-problems/ChocolatesDistribution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+public class ChocolatesDistribution{
+		
+	public static int[] findRemainderAndQuotient(int number, int divisor){
+		int[] arr = new int[2];
+		
+		
+		arr[0] =  number%divisor;
+		arr[1] = number/divisor;
+		return arr;
+	}	
+		
+
+	public static void main(String args[]){
+		Scanner s = new Scanner(System.in);
+	
+		//take input
+		System.out.println("Enter the the number of chocolates and children  : ");
+		int n1 = s.nextInt();
+		int n2 = s.nextInt();
+		
+		
+		int[] resultArr = new int [2];
+		resultArr = findRemainderAndQuotient(n1,n2);
+		
+		// print output
+		System.out.println("remaining and child get chocolates are respectively :  ");
+		for(int i=0; i<2; i++){
+			System.out.print(resultArr[i] +" ");
+		}
+	}
+}
