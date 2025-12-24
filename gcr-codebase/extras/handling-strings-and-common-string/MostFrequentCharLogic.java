e480a49586fce1821e7feb30d31c221464bbaca4
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-24 11:28:36 +0530

    [Sunny] : added Most Frequent char Logic

---
+++ gcr-codebase/extras/handling-strings-and-common-string/MostFrequentCharLogic.java
@@ -0,0 +1,25 @@
+public class MostFrequentCharLogic {
+
+    public static void main(String[] args) {
+        String str = "programming";
+        char maxChar = str.charAt(0);
+        int maxCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count > maxCount) {
+                maxCount = count;
+                maxChar = str.charAt(i);
+            }
+        }
+
+        System.out.println("Most frequent character: " + maxChar);
+    }
+}
