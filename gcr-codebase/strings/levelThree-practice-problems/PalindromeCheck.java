5fb68a5d1f4e150444232ab07f1efeeb8f824f3e
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-24 08:56:38 +0530

    [Sunny] : added Palindrome Check

---
+++ gcr-codebase/strings/levelThree-practice-problems/PalindromeCheck.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+public class PalindromeCheck {
+
+    //Iterative comparison using charAt()
+    public static boolean isPalindromeIterative(String text) {
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    //  Recursive comparison using charAt()
+    public static boolean isPalindromeRecursive(String text, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (text.charAt(start) != text.charAt(end)) {
+            return false;
+        }
+        return isPalindromeRecursive(text, start + 1, end - 1);
+    }
+
+    // Method to reverse string using charAt()
+    public static char[] reverseString(String text) {
+        char[] reverse = new char[text.length()];
+        int index = 0;
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reverse[index++] = text.charAt(i);
+        }
+        return reverse;
+    }
+
+    // Reverse and compare 
+    public static boolean isPalindromeUsingArray(String text) {
+        char[] original = text.toCharArray();
+        char[] reverse = reverseString(text);
+
+        for (int i = 0; i < original.length; i++) {
+            if (original[i] != reverse[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Create Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // Take user input
+        System.out.println("Enter a text:");
+        String input = sc.nextLine();
+
+        boolean result1 = isPalindromeIterative(input);
+        boolean result2 = isPalindromeRecursive(input, 0, input.length() - 1);
+        boolean result3 = isPalindromeUsingArray(input);
+
+        System.out.println("\nPalindrome Check Results:");
+        System.out.println("Logic 1 (Iterative): " + result1);
+        System.out.println("Logic 2 (Recursive): " + result2);
+        System.out.println("Logic 3 (Array): " + result3);
+    }
+}
