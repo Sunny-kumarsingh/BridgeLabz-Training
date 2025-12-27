f30bfd5eb510b07b66578dd58fc2d52f6d595593
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-28 05:07:38 +0530

    [sunny] : Added : this file contain main method of Book class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/BookMain.java
@@ -0,0 +1,15 @@
+package com.constructors.levelone;
+
+public class BookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Book b =new Book();
+		 b.display();
+		 
+	     Book b2 = new Book("Java Programming", "James Gosling", 500);
+	     b2.display();
+
+	}
+
+}
