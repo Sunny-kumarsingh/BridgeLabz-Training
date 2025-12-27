a3b9ca4e619ebd92321c18b8fc85d04a0691b32a
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-27 23:50:23 +0530

    [sunny] : Added : Bank Account

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/BankAccount.java
@@ -0,0 +1,40 @@
+package com.constructors.levelone;
+
+public class BankAccount {
+	// Public variable
+    public int accountNumber;
+
+    // Protected variable
+    protected String accountHolder;
+
+    // Private variable
+    private double balance;
+
+    // Constructor
+    BankAccount(int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    // Public method to get balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Public method to deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    // Public method to withdraw money
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+}
