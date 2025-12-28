6f3f4b04580b5e6ed1f69a1c750ae22df4417f83
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2025-12-28 11:46:21 +0530

    [sunny] : Added : carRental main method

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/carRentalMain.java
@@ -0,0 +1,11 @@
+package com.constructors.levelone;
+
+public class carRentalMain {
+	public static void main(String[] args) {
+
+        // Using parameterized constructor
+        CarRental r2 = new CarRental("Raj", "Swift", 5);
+        r2.display();
+        
+    }
+}
