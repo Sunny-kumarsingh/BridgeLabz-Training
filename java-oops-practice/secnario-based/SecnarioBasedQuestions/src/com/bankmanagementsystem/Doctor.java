f04e8dfff78e141e10bc761b7f4ace1cf34306c3
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-03 01:13:19 +0530

    [Sunny]: update : modify path

--- java-oops-practice/secnario-based/SecnarioBasedQuestions/src/com/bankmanagementsystem/Doctor.java
+++ java-oops-practice/secnario-based/SecnarioBasedQuestions/src/com/bankmanagementsystem/Doctor.java
@@ -2,4 +2,15 @@ package com.bankmanagementsystem;
 
 public class Doctor {
 
+    String name;
+    String specialization;
+
+    Doctor(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    void displayInfo() {
+        System.out.println("Doctor: " + name + ", Specialization: " + specialization);
+    }
 }
