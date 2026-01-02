740dfe952e060924b75db864700ec2e7dd76d2ce
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-02 22:47:06 +0530

    [Sunny]: Upadate : Some update

--- java-oops-practice/secnario-based/SecnarioBasedQuestions/src/com/bankmanagementsystem/Bill.java
+++ java-oops-practice/secnario-based/SecnarioBasedQuestions/src/com/bankmanagementsystem/Bill.java
@@ -2,4 +2,20 @@ package com.bankmanagementsystem;
 
 public class Bill {
 
+    double consultationFee;
+    double medicineCost;
+    double taxRate = 0.05;  
+    double discount = 0.10;  
+
+    Bill(double consultationFee, double medicineCost) {
+        this.consultationFee = consultationFee;
+        this.medicineCost = medicineCost;
+    }
+
+    double calculatePayment() {
+        double total = consultationFee + medicineCost;
+        double tax = total * taxRate;
+        double discountAmount = total * discount;
+        return total + tax - discountAmount;
+    }
 }
