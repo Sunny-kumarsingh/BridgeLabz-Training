541957abc67340ad3b53eb18935eff064b627d6e
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-02 12:04:43 +0530

    [sunny] : Added :  online retail order management

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/onlineretailordermanagement/OrderMain.java
@@ -0,0 +1,18 @@
+package com.inheritance.multilevelinheritance;
+
+public class OrderMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 	Order o1 = new Order(101, "01-09-2025");
+	        Order o2 = new ShippedOrder(102, "02-09-2025", "TRK123");
+	        Order o3 = new DeliveredOrder(103, "03-09-2025", "TRK456", "06-09-2025");
+
+	        System.out.println(o1.getOrderStatus());
+	        System.out.println(o2.getOrderStatus());
+	        System.out.println(o3.getOrderStatus());
+
+	}
+
+}
