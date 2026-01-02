193c5f0f3477686b69930d2f4c7941bcd4707c99
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-02 11:25:16 +0530

    [sunny] : Added : Manager class inherit the Employee class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblem/Manager.java
@@ -0,0 +1,15 @@
+package com.inheritance.assistedproblem;
+
+public class Manager extends Employee {
+	int teamSize;
+	
+	Manager(String name, int id, int salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+	
+	void displayDetails(){
+		super.displayDetails();
+		System.out.println("team Size : " + teamSize);
+	}
+}
