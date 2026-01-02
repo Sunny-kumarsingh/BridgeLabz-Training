75f4c60e897d102c674eab931196e0b18792edac
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-02 17:13:45 +0530

    [Sunny]: Online Food Delivery System

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstract/onlinefooddeliverysystem/Discountable.java
@@ -0,0 +1,6 @@
+package com.encapsulationpolymorphisminterfaceandabstract.onlinefooddeliverysystem;
+
+interface Discountable {
+    double applyDiscount();
+    String getDiscountDetails();
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstract/onlinefooddeliverysystem/FoodItem.java
@@ -0,0 +1,35 @@
+package com.encapsulationpolymorphisminterfaceandabstract.onlinefooddeliverysystem;
+
+abstract class FoodItem {
+
+    // Encapsulation
+    private String itemName;
+    private double price;
+    private int quantity;
+
+    // Constructor
+    public FoodItem(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Abstract method
+    public abstract double calculateTotalPrice();
+
+    // Concrete method
+    public void getItemDetails() {
+        System.out.println("Item Name : " + itemName);
+        System.out.println("Price     : " + price);
+        System.out.println("Quantity  : " + quantity);
+    }
+
+    // Protected access for subclasses
+    protected double getPrice() {
+        return price;
+    }
+
+    protected int getQuantity() {
+        return quantity;
+    }
+}
