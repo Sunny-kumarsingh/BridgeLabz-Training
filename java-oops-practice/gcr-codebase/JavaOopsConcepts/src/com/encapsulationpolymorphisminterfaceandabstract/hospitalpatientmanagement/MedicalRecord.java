9eaaa51471e5fafc11b54b906e395954a124e122
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-02 16:49:56 +0530

    [Sunny]: Hospital patient management

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstract/hospitalpatientmanagement/MedicalRecord.java
@@ -0,0 +1,7 @@
+package com.encapsulationpolymorphisminterfaceandabstract.hospitalpatientmanagement;
+
+interface MedicalRecord {
+    void addRecord(String diagnosis);
+    void viewRecords();
+}
+
