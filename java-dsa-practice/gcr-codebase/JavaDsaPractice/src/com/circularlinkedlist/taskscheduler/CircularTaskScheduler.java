b2ab1fa2e99fb38e6a14bdb0fdc29cc9755e349c
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-05 00:35:04 +0530

    [sunny]: Added : Task scheduler

---
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/circularlinkedlist/taskscheduler/CircularTaskScheduler.java
@@ -0,0 +1,29 @@
+package com.circularlinkedlist.taskscheduler;
+
+public class CircularTaskScheduler {
+    public static void main(String[] args) {
+
+        TaskScheduler scheduler = new TaskScheduler();
+
+        scheduler.addAtEnd(1, "Write Code", 1, "2026-01-10");
+        scheduler.addAtEnd(2, "Encoding", 2, "2026-01-12");
+        scheduler.addAtBeginning(3, "Fix Bugs", 1, "2026-01-08");
+        scheduler.addAtPosition(2, 4, "Testing", 3, "2026-01-11");
+
+        scheduler.displayAllTasks();
+
+        System.out.println("\n--- Current Task ---");
+        scheduler.viewCurrentTask();
+
+        System.out.println("\n--- Move to Next Task ---");
+        scheduler.moveToNextTask();
+        scheduler.viewCurrentTask();
+
+        System.out.println("\n--- Search by Priority (1) ---");
+        scheduler.searchByPriority(1);
+
+        System.out.println("\n--- Remove Task (ID = 2) ---");
+        scheduler.removeByTaskId(2);
+        scheduler.displayAllTasks();
+    }
+}
