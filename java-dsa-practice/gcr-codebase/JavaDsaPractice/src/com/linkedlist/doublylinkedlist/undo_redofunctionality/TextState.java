df0257952ccc41ca4547604783de4bfcfce73ac0
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-06 15:04:36 +0530

    [sunny]: Added : undo_redofunctionality

---
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/doublylinkedlist/undo_redofunctionality/TextState.java
@@ -0,0 +1,13 @@
+package com.linkedlist.doublylinkedlist.undo_redofunctionality;
+
+public class TextState {
+    String content;
+    TextState prev;
+    TextState next;
+
+    TextState(String content) {
+        this.content = content;
+        this.prev = null;
+        this.next = null;
+    }
+}
