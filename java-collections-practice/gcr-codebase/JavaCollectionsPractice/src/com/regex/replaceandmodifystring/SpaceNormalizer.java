0d14749d7b0b75c75bc9419cc4c1a0cbdabc52af
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-26 13:20:46 +0530

    [sunny] Added : replace extra space with single space

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/regex/replaceandmodifystring/SpaceNormalizer.java
@@ -0,0 +1,12 @@
+package com.regex.replaceandmodifystring;
+public class SpaceNormalizer {
+
+    public static void main(String[] args) {
+
+        String text = "This   is   an   example    with   multiple   spaces.";
+
+        String result = text.replaceAll("\\s+", " ");
+
+        System.out.println(result);
+    }
+}
