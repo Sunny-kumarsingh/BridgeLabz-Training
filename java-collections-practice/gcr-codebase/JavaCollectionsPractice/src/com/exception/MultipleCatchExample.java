e09c7bdec90bd96a605547a618af9ff1ff154635
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-26 13:49:19 +0530

    [sunny] Added :  multiple catch block

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exception/MultipleCatchExample.java
@@ -0,0 +1,25 @@
+package com.exception;
+import java.util.Scanner;
+
+public class MultipleCatchExample {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int[] arr = {10, 20, 30, 40, 50};
+
+            System.out.print("Enter index: ");
+            int index = sc.nextInt();
+
+            System.out.println("Value at index " + index + ": " + arr[index]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
