164958e80b267111aa0f92ab8c82ebdcfcd85cab
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-26 14:08:19 +0530

    [sunny] Added :  differnce between throw and throws

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exception/ThrowVsThrowsExample.java
@@ -0,0 +1,23 @@
+package com.exception;
+public class ThrowVsThrowsExample {
+
+    static double calculateInterest(double amount, double rate, int years)throws IllegalArgumentException {
+
+        if (amount < 0 || rate < 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        return (amount * rate * years) / 100;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            double interest = calculateInterest(10000, 5, 2);
+            System.out.println("Calculated Interest: " + interest);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid input: Amount and rate must be positive");
+        }
+    }
+}
