4515a59e706e140dba39c741c8316108ff22e0fd
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-26 14:10:47 +0530

    [sunny] Added :  try with resource

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exception/TryWithResourcesExample.java
@@ -0,0 +1,21 @@
+package com.exception;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResourcesExample {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+
+            String line = br.readLine();
+            if (line != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
