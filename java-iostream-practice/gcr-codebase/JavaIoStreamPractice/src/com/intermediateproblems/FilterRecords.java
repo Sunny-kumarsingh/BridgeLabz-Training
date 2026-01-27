228781f08eab131eaed06aef82168fca096d7a97
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-27 14:53:08 +0530

    [sunny] Added : Filter record from csv file

---
+++ java-iostream-practice/gcr-codebase/JavaIoStreamPractice/src/com/intermediateproblems/FilterRecords.java
@@ -0,0 +1,36 @@
+package com.intermediateproblems;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FilterRecords {
+	
+	public static void main(String[] args) {
+		
+        String filePath =
+        		"C:\\Users\\sunny\\Desktop\\Java programming workspace\\java-iostream-practice\\gcr-codebase\\JavaIoStreamPractice\\src\\sampleCSVfile\\studentsFilter.txt";
+        
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+        	
+        	br.readLine();
+        	String line;
+        	while((line = br.readLine())!= null) {
+        		
+        		String[] data = line.split(",");
+        		String name = data[0];
+        		int marks = Integer.parseInt(data[1]);
+        		
+        		if(marks >= 80) {
+        			System.out.println(name + "  " + marks);
+        		}
+         		
+        	}
+        	br.close();
+        	
+        }catch(IOException e) {
+        	e.printStackTrace();
+        }
+
+	}
+
+}
