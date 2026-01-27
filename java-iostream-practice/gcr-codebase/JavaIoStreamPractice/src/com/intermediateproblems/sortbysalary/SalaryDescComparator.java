a678063d48693841a37fa12e098a36a55a0fcd19
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-27 20:39:14 +0530

    [sunny] Added sort by salary

---
+++ java-iostream-practice/gcr-codebase/JavaIoStreamPractice/src/com/intermediateproblems/sortbysalary/SalaryDescComparator.java
@@ -0,0 +1,11 @@
+package com.intermediateproblems.sortbysalary;
+
+import java.util.Comparator;
+
+public class SalaryDescComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        return e2.getSalary() - e1.getSalary(); 
+    }
+}
