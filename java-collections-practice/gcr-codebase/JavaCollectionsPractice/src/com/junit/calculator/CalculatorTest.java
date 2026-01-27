fea2e81e130c1b33f3cb4e3ddfbcf2c07f646637
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-27 12:31:13 +0530

    [sunny] added junit testing of calculator

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/junit/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+
+
+package com.junit.calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(10, calculator.add(5, 5));
+    }
+}
+
