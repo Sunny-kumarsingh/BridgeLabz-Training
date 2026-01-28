c65373cf872da916c3d62dc4a81cd4413a5ee822
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-01-28 11:48:03 +0530

    [sunny] Added : Sort by the salary

---
+++ java-iostream-practice/gcr-codebase/JavaIoStreamPractice/src/com/csvData/intermediateproblems/sortbysalary/Employee.java
@@ -0,0 +1,26 @@
+package com.csvData.intermediateproblems.sortbysalary;
+
+public class Employee {
+
+    private String name;
+    private String department;
+    private int salary;
+
+    public Employee(String name, String department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
---
+++ java-iostream-practice/gcr-codebase/JavaIoStreamPractice/src/com/csvData/intermediateproblems/sortbysalary/SortCSVBySalary.java
@@ -0,0 +1,58 @@
+package com.csvData.intermediateproblems.sortbysalary;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortCSVBySalary {
+
+    public static void main(String[] args) {
+
+        String inputFile =
+            "C:\\Users\\sunny\\Desktop\\Java programming workspace\\java-iostream-practice\\gcr-codebase\\JavaIoStreamPractice\\src\\sampleCSVfile\\employees.csv";
+
+        List<Employee> employees = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+
+            String line;
+
+            // skip header
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",");
+
+                employees.add(
+                    new Employee(
+                        data[1],                  
+                        data[2],                 
+                        Integer.parseInt(data[3]) 
+                    )
+                );
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // sort by salary DESE
+        Collections.sort(employees, new SalaryDescComparator());
+
+        System.out.println("Top 5 Highest Paid Employees");
+        System.out.println("-----------------------------");
+
+        for (int i = 0; i < Math.min(5, employees.size()); i++) {
+            Employee e = employees.get(i);
+            System.out.println(
+                e.getName() + " | " +
+                e.getDepartment() + " | " +
+                e.getSalary()
+            );
+        }
+    }
+}
