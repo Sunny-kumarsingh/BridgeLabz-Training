b625f4b63abe0dcce3f2fdbcc52decd25528ab8c
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-02-01 13:21:30 +0530

    [sunny] Added : giving Alert

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/lambdaexpressions/Alert.java
@@ -0,0 +1,16 @@
+package com.lambdaexpressions;
+
+public class Alert {
+	
+	String message;
+	String type;
+	
+	Alert(String message, String type){
+		this.message = message;
+		this.type = type;
+	}
+	
+	public String toString() {
+		return type + ":" + message;
+	}
+}
