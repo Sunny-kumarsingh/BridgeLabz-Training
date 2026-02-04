8b79f15349c2ed79a09111943f9bcab37b231643
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-02-04 12:00:40 +0530

    [sunny] Added  download manager

---
+++ java-design-pattern-practice/gcr-codebase/JavaDesignPatternPractice/src/com/multithreding/downloadmanager/FileDownloaderThread.java
@@ -0,0 +1,39 @@
+package com.multithreding.downloadmanager;
+import java.util.Random;
+
+public class FileDownloaderThread extends Thread {
+	private String fileName;
+	private Random random = new Random();
+	
+	
+	 FileDownloaderThread(String fileName) {
+	        this.fileName = fileName;
+	 }
+	 
+	 @Override
+	 public void run() {
+		 int progress = 0;
+
+	        while (progress < 100) {
+
+	            // random progress increase (10–30)
+	            int increment = 10 + random.nextInt(21);
+	            progress += increment;
+
+	            if (progress > 100) {
+	                progress = 100;
+	            }
+
+	            System.out.println("[" + Thread.currentThread().getName() +
+	                    "] Downloading " + fileName + " : " + progress + "%");
+
+	            try {
+	                // random delay
+	                Thread.sleep(500 + random.nextInt(700));
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+		 
+	 }
+}
